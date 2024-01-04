a3b421dcfc4ef42ed0dd7799dc2cbbb0f5b7a7c2
Ahmad Mustafa <47633003+amustafa9@users.noreply.github.com>
2024-01-04 08:36:51 -0600

    argparser handles variable keyword arguments

--- scripts/main.py
+++ scripts/main.py
@@ -3,20 +3,25 @@ from seismic_slicer import SeismicSlicer
 
 
 def main():
-    # # Create ArgumentParser object
-    # parser = argparse.ArgumentParser(description='Program running RegressNet Horizon Tracker')
-    #
-    # # Add optional argument
-    # parser.add_argument('-i', '--input_file', required=True, help='Path to the configuration file')
-    # parser.add_argument('-f', '--fast_scan', required=True, help='Set to True if fast scanning of segy file is desired')
-    #
-    # # Parse the command line arguments
-    # args = parser.parse_args()
+    # Create ArgumentParser object
+    parser = argparse.ArgumentParser(description='Program running RegressNet Horizon Tracker')
 
-    seismic_slicer = SeismicSlicer(r"E:\seismic_volumes_python\input_cubes\169394_11.lto.RTM_enhanced_stack_OBN_TTI.6586761042_input.segy",
-                                   horizon_file_path=r"E:\seismic_volumes_python\result_compare")
+    # Add arguments
+    parser.add_argument('-i', '--input_file', required=True, help='Path to the configuration file')
+    parser.add_argument('-hor', '--horizon_path', required=False, help='Path to a specific horizon picks file or a directory containing multiple horizon files')
+    parser.add_argument('-f', '--fast_scan', required=False, help='Set to True if fast scanning of segy file is desired')
 
-    # seismic_slicer = SeismicSlicer(r"E:\seismic_volumes_python\input_cubes\169394_11.lto.RTM_enhanced_stack_OBN_TTI.6586761042_input.segy")
+    # Parse the command line arguments
+    args = parser.parse_args()
+
+    # extract values of the arguments
+    path_segy = args.input_file  # path to segy file
+
+    # construct keyword argument dictionary
+    keyword_dict = {'horizon_file_path': args.horizon_path}
+
+    # initialize seismic slicer with the user-supplied arguments
+    seismic_slicer = SeismicSlicer(path_segy=path_segy, fast_scan=True, **keyword_dict)
 
 
 if __name__ == "__main__":
