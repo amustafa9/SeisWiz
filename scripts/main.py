b30ac68e4f3e75d46486efd1d008d0ad8ec1ff3d
Ahmad Mustafa <47633003+amustafa9@users.noreply.github.com>
2024-01-19 09:39:49 -0600

    Update main.py
    added argument for loading an attribute volume file

--- scripts/main.py
+++ scripts/main.py
@@ -7,9 +7,10 @@ def main():
     parser = argparse.ArgumentParser(description='Program running RegressNet Horizon Tracker')
 
     # Add arguments
-    parser.add_argument('-i', '--input_file', required=True, help='Path to the configuration file')
+    parser.add_argument('-i', '--input_file', required=True, help='Path to the input segy file')
     parser.add_argument('-hor', '--horizon_path', required=False, help='Path to a specific horizon picks file or a directory containing multiple horizon files')
     parser.add_argument('-cmap', '--color_map', required=False, help='Colormap to render the seismic data. Must be a matplotlib compatible.')
+    parser.add_argument('-att', '--attribute_volume_file', required=False, help='Path to an attribute segy file that needs be overlaid on the original seismic')
 
     # Parse the command line arguments
     args = parser.parse_args()
@@ -19,7 +20,8 @@ def main():
 
     # construct keyword argument dictionary to handle optional arguments
     keyword_dict = {'horizon_file_path': args.horizon_path,
-                    'cmap': args.color_map}
+                    'cmap': args.color_map,
+                    'att': args.attribute_volume_file}
 
     # initialize seismic slicer with the user-supplied arguments
     seismic_slicer = SeismicSlicer(path_segy=path_segy, **keyword_dict)
